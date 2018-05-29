ed4657b356e89c9c10ffd4fd2767fdd46ac8116f
popologu <popologu@gmail.com>
2018-05-29 20:01:46 +0900

    데이타 커밋

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] gameGameras;
+
+    private int gameCameraIndex = 2222222;
+
+    void Start () {
+        FocusOnCamera (gameCameraIndex);
+    }
+	
+	
+	void Update () {
+        if(Input.GetMouseButtonDown (0)) {
+            ChangeCamera (1);
+        }
+        }
+
+    void FocusOnCamera(int index) {
+        for(int i=0; i < gameGameras.Length; i++) {
+            gameGameras[i].SetActive (i == index);
+        }
+    }
+    
+    void ChangeCamera (int direction) {
+        gameCameraIndex += direction;
+
+        if(gameCameraIndex >= gameGameras.Length) {
+            gameCameraIndex = 0;
+           }
+
+        if(gameCameraIndex <= 0) {
+            gameCameraIndex = gameGameras.Length - 1;
+        }
+
+        FocusOnCamera (gameCameraIndex);
+
+    }
+   
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float speed = 3f;
+    public float rotatingSpeed = 40f;
+    public float jumpingForce = 10f;
+
+    private bool canJump = false;
+
+    
+    void Start () {
+     
+    }
+
+
+    void Update () {
+        if (Input.GetKey ("right")) {
+            transform.RotateAround (transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey ("left")) {
+            transform.RotateAround (transform.position, Vector3.up, -rotatingSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("up")) {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+
+        if (Input.GetKeyDown ("space") && canJump) {
+            canJump = false;
+           GetComponent<Rigidbody>().AddForce (0, jumpingForce, 0);
+        }
+
+    }
+
+    void OnCollisionEnter (Collision collision) {
+
+        if (collision.transform.name == "Floor") {
+            canJump = true;
+        }
+    }
+}
